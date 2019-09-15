165b7d294ff7167ebce585850f68d9d3814e79e8
sahar399 <shar.bahman@gmail.com>
2019-09-15 14:28:48 +0430

    Updates

---
+++ precache-manifest.c85bd945786aa7de50aa4ede836d5620.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be349e6b2afd2481a76c0cd218115a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd423d8f40ea911fae9e",
+    "url": "/static/css/main.6dcf297a.chunk.css"
+  },
+  {
+    "revision": "58f69fe254f120b2f8f8",
+    "url": "/static/js/2.8d947b8f.chunk.js"
+  },
+  {
+    "revision": "cd423d8f40ea911fae9e",
+    "url": "/static/js/main.9ccd94dd.chunk.js"
+  },
+  {
+    "revision": "bdac357fef078c279797",
+    "url": "/static/js/runtime~main.8fba62f2.js"
+  }
+]);
