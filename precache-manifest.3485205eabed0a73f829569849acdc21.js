25bd011f8b9ce0dc6578c278f22e36c85bdcc842
sahar399 <shar.bahman@gmail.com>
2019-09-16 10:50:31 +0430

    Updates

---
+++ precache-manifest.3485205eabed0a73f829569849acdc21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eff19274b1d9f79447073b545c27f60",
+    "url": "/maktabkhoone-resum/index.html"
+  },
+  {
+    "revision": "0b919c5ac88f7b50c8b2",
+    "url": "/maktabkhoone-resum/static/css/main.6dcf297a.chunk.css"
+  },
+  {
+    "revision": "58f69fe254f120b2f8f8",
+    "url": "/maktabkhoone-resum/static/js/2.8d947b8f.chunk.js"
+  },
+  {
+    "revision": "0b919c5ac88f7b50c8b2",
+    "url": "/maktabkhoone-resum/static/js/main.9ca31d36.chunk.js"
+  },
+  {
+    "revision": "fcf44a9bd5c37772a519",
+    "url": "/maktabkhoone-resum/static/js/runtime~main.78d13051.js"
+  }
+]);
